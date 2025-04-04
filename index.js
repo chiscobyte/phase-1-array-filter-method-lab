38e13bb029ac42cec686f42ff0955b82932d6d3b
Enock Chisiwa <enock.chisiwa@student.moringaschool.com>
2025-04-04 11:22:45 +0300

    initiate

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name){
+    const matching = drivers.filter(item => item.toUpperCase() === name.toUpperCase())
+    return matching;
+}
 
-function findMatching(collection, name){
-    const matchingNames = []
-    for(const user of collection){
-        if(user.toLowerCase() ===  name.toLowerCase()){
-            matchingNames.push(name)
-        }
-    }
 
-    return matchingNames
+function fuzzyMatch(drivers,letter){
+const count = letter.length;
+const matchStart =  drivers.filter(item => item.substring(0,count) === letter);
+console.log(matchStart)
+return matchStart;
+}
+
+
+function matchName(driver, test){
+const nameSimilar = driver.filter(item => item.name === test);
+return nameSimilar;
 }
