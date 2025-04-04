24317078c1a83ca47267c4b92967cedf596a8525
Enock Chisiwa <enock.chisiwa@student.moringaschool.com>
2025-04-04 11:09:43 +0300

    initiate

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, name){
+    const matchingNames = []
+    for(const user of collection){
+        if(user.toLowerCase() ===  name.toLowerCase()){
+            matchingNames.push(name)
+        }
+    }
+
+    return matchingNames
+}
